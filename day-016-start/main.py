d1fc49b596876b60f929d47b61d50598ffce95ac
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2023-01-29 11:10:46 -0500

    Initial commit

---
+++ day-016-start/main.py
@@ -0,0 +1,26 @@
+# import another_module
+# print(another_module.another_variable)
+
+# classの頭文字は大文字
+# turtle graphics library
+# https://docs.python.org/3/library/turtle.html
+# from turtle import Turtle, Screen
+#
+# # turtle graphics libraryのTurtle classを使って新しい亀オブジェクトを作成
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkOrange")
+# timmy.forward(100)
+#
+# # turtle graphics libraryのScreen classを使って新しいオブジェクトを作成
+# my_screen = Screen()
+# # my_screen is object and canvheight is attribute associate the object
+# print(my_screen.canvheight)
+# # function
+# # クリックするまで実行されるfunction
+# my_screen.exitonclick()
+
+import prettytable
+
+
