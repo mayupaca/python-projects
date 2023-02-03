5a46753071257e665c92505ac8a0850c5b4f3bfb
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2023-02-03 13:15:45 -0500

    dots painting project

--- 018-hirst-painting/main.py
+++ 018-hirst-painting/main.py
@@ -13,25 +13,36 @@
 # print(rgb_list)
 
 from turtle import Turtle, Screen
+import turtle as turtle_module
 import random
 
-tim = Turtle()
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
 
 color_list = [(197, 165, 117), (142, 80, 56), (220, 201, 137), (59, 94, 119), (164, 152, 53), (136, 162, 181), (131, 34, 22), (69, 39, 32), (53, 117, 86), (192, 95, 78), (146, 177, 149), (19, 91, 68), (165, 143, 157), (31, 59, 76), (111, 75, 81), (228, 176, 164), (128, 29, 33), (179, 204, 177), (71, 34, 36), (25, 82, 89), (89, 146, 127), (18, 69, 57), (41, 66, 90), (219, 178, 182), (175, 94, 98), (179, 192, 205)]
 
+tim.setheading(225)
+tim.forward(250)
+tim.setheading(0)
+number_of_dots = 100
 
-for row in range(1, 11):
-    tim.penup()
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
     tim.forward(50)
-    tim.dot(20)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
-# tim.right(90)
-# tim.forward(80)
-# tim.right(90)
 
-    for column in range(2, 11):
-        tim.penup()
-        tim.forward(80)
+    # for column in range(2, 11):
+    #     tim.penup()
+    #     tim.forward(80)
 
 
 # column
